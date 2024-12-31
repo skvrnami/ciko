d8c1b677b505c74b7ebe568054ef82e5bb53e038
Michael Škvrňák <michael.skvrnak@protonmail.com>
2024-12-31 14:33:22 +0100

    Add parse pocket script

---
+++ django/parse_pocket.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd
+import pocket as pckt
+import datetime as dt
+import trafilatura as tf
+import django
+
+from django.db.utils import IntegrityError
+from ollama_summarise import summarise_text
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
+
+from read.models import Text
+
+def convert_timestamp_to_date(x):
+    d = dt.datetime.fromtimestamp(int(x), dt.UTC) 
+    return d.strftime("%Y-%m-%dT%H:%M:%S+00:00")
+
+p = pckt.Pocket(
+    consumer_key = os.environ["POCKET_CONSUMER_KEY"],
+    access_token = os.environ["POCKET_ACCESS_TOKEN"]
+)
+
+pckt_articles = p.retrieve(count=10, state="unread")
+pckt_articles_df = pd.DataFrame.from_dict(pckt_articles["list"], orient = "index")
+
+pckt_articles_df["published"] = pckt_articles_df["time_added"].apply(convert_timestamp_to_date)
+pckt_articles_df = pckt_articles_df.rename(columns = {"resolved_url":"link", "resolved_title":"title", "excerpt":"summary"})
+pckt_articles_df_trim = pckt_articles_df[["published", "link", "title", "summary", "item_id"]]
+
+for _, row in pckt_articles_df_trim.iterrows():
+    try:
+        html = tf.fetch_url(row["link"])
+        txt = tf.extract(html, output_format = "html", include_links=True, include_images=True, include_formatting=True)
+        metadata = tf.extract_metadata(html).as_dict()
+        summary = summarise_text(txt)
+    except:
+        try:
+            txt = tf.extract(html, output_format = "html", include_images=True)
+            metadata = tf.extract_metadata(html).as_dict()
+            summary = summarise_text(txt)
+        except:
+            txt = tf.extract(html)
+            metadata = tf.extract_metadata(html).as_dict()
+            summary = summarise_text(txt)
+
+
+    t = Text(link = row["link"], publication_date = row["published"], author = metadata["author"], title = row["title"], 
+             summary = summary, content = txt, source = metadata["sitename"])
+    try:
+        print(t)
+        t.save()
+        p.archive(row["item_id"]).commit()
+    except IntegrityError:
+        print("oops ", row["title"], " is already in the DB")
+        p.archive(row["item_id"]).commit()
+
